e49bfc49216512b191e7d7d8e75e5d48424e0077
Ryan <ryanpburnett@yahoo.com>
2022-01-21 09:54:01 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './card/card.component';
+import { MainComponent } from './main/main.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CardComponent,
+    MainComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
